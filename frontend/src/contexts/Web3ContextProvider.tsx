5802cc5c6295fa6cf045d807b1aebe46e4732b51
Johnson Tinuoye <johnsontinuoye@gmail.com>
2023-02-21 11:48:27 +0100

    Setup contexts

---
+++ frontend/src/contexts/Web3ContextProvider.tsx
@@ -0,0 +1,59 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNameFee, getCurrentBlock, web3 } from "../lib/web3-config";
+
+interface IWeb3Context {
+  accounts: string[];
+  nameFee?: number;
+  currentBlock?: number;
+  account: string | null;
+  setAccount: (account: string) => void;
+}
+
+export const Web3Context = createContext<IWeb3Context>({
+  accounts: [],
+  account: null,
+  setAccount: () => null,
+});
+
+export default function Web3ContextProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const [account, setAccount] = useState<string | null>(null);
+
+  const { data: nameFee } = useQuery({
+    queryKey: ["name_fee"],
+    queryFn: fetchNameFee,
+  });
+  const { data: currentBlock } = useQuery({
+    queryKey: ["current_Block"],
+    queryFn: getCurrentBlock,
+  });
+
+  useEffect(() => {
+    accounts?.length && setAccount(accounts[0]);
+  }, [accounts]);
+
+  useEffect(() => {
+    web3.eth.requestAccounts().then((items) => {
+      setAccounts(items);
+    });
+  }, []);
+
+  return (
+    <Web3Context.Provider
+      value={{
+        accounts,
+        currentBlock,
+        nameFee,
+        account,
+        setAccount,
+      }}
+    >
+      {children}
+    </Web3Context.Provider>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Web3ContextProvider from "./contexts/Web3ContextProvider";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App";
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchInterval: 20000,
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Web3ContextProvider>
+        <App />
+      </Web3ContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
